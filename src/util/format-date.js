dda41304ff99f5b69d9b1872922d0acdb3fac104
paul-mcn <paul.a.mcnamara@outlook.com>
2023-07-18 11:34:38 +1000

    removing old project

--- src/util/format-date.js
+++
@@ -1,7 +0,0 @@
-export const getDate = (...args) => {
-    return new Date(...args);
-}
-
-export const getStartOfWeek = () => {
-    console.log(getDate());
-}
--- src/util/format-input.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string} searchTerm 
- * @returns string without special characters
- */
-export const removeSpecialCharactersFromString = (searchTerm) => {
-    // replace special characters with empty character
-    return searchTerm.replace(/[~`!@#$%^&*()+={}[\];:'"<>.,\\?]/g, '')
-}
