01c1590754e98ddcdac078231faefc26d2466cb9
paul-mcn <paul.a.mcnamara@outlook.com>
2024-03-27 14:08:02 +1100

    added styling for better ux

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,22 +1,29 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const router = usePathname();
 
-	return (
-		<div className="bg-white w-full h-20">
-			<ul className="flex flex-row items-center text-gray-900 gap-4 mt-auto h-full w-4/5 mx-auto font-bold">
-				<li className="">
-					<Link href="/">Home</Link>
-				</li>
-				<li className="">
-					<Link href="/meals">Meals</Link>
-				</li>
-				<li className="">
-					<Link href="/"></Link>
-				</li>
-			</ul>
-		</div>
-	);
+  const links = [
+    { name: "Home", href: "/" },
+    { name: "Meals", href: "/meals" },
+  ];
+
+  return (
+    <div className="bg-white w-full h-20">
+      <ul className="flex flex-row items-center text-gray-900 gap-4 mt-auto h-full w-4/5 mx-auto font-bold">
+        {links.map((link) => (
+          <li
+            className={`${router === link.href ? "text-green-600" : "hover:opacity-80"}`}
+            key={link.href}
+          >
+            <Link href={link.href}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default NavBar;
