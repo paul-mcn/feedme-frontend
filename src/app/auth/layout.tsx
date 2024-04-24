f29ec37d2336206123715a76e98eefef1a1dd3f2
paul-mcn <paul.a.mcnamara@outlook.com>
2024-04-24 13:53:28 +1000

    removed zombie code, added types, formatted

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,15 +1,14 @@
-import { cookies } from "next/headers";
 import React from "react";
 
 export default function AuthLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<div className="bg-gray-100">
-			<div className="w-4/5 mx-auto py-16">{children}</div>
-		</div>
-	);
-}
 
+  return (
+    <div className="bg-gray-100">
+      <div className="w-4/5 mx-auto py-16">{children}</div>
+    </div>
+  );
+}
