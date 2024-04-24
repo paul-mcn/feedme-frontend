fe24bd35915d369efa63f0f67cbc2aaac9442125
paul-mcn <paul.a.mcnamara@outlook.com>
2024-04-24 13:52:29 +1000

    fix set cookie only if user is auth

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,27 +2,31 @@ import { serialize } from "cookie";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-	const formData = await req.formData()
+  const formData = await req.formData();
 
-	const tokenPayload = await fetch('http://localhost:8000/auth/token', {
-		method: 'POST',
-		body: formData
-	})
+  const tokenPayload = await fetch("http://localhost:8000/auth/token", {
+    method: "POST",
+    body: formData,
+  });
 
-	const token = await tokenPayload.json()
+  if (tokenPayload.statusText === "Unauthorized") {
+    return tokenPayload;
+  }
 
-	// Below is an example of saving information to a cookie
-	const cookie = serialize('token', JSON.stringify(token), {
-		httpOnly: true,
-		secure: process.env.NODE_ENV === 'production',
-		maxAge: 60 * 60 * 24 * 7, // One week
-		path: '/',
-	})
+  const token = await tokenPayload.json();
 
-	return new NextResponse("Successfully set cookie!", {
-		status: 200,
-		headers: {
-			'Set-Cookie': cookie,
-		}
-	})
+  // Below is an example of saving information to a cookie
+  const cookie = serialize("token", JSON.stringify(token), {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 7, // One week
+    path: "/",
+  });
+
+  return new NextResponse("Successfully set cookie!", {
+    status: 200,
+    headers: {
+      "Set-Cookie": cookie,
+    },
+  });
 }
