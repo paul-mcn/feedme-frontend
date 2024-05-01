4d93082fac6e57d130b6a381068973015204185c
paul-mcn <paul.a.mcnamara@outlook.com>
2024-05-01 10:16:43 +1000

    Removed zombie code

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -1,31 +1,13 @@
-import useGetMeals from "@/hooks/meals";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 import Meals from "./meals";
 import LinkButton from "@/components/buttons/Link";
 
 export default async function MealsPage() {
-  // TODO implement prefetching query
-  // const queryClient = new QueryClient();
-
-  // await queryClient.prefetchQuery({
-  // 	queryKey: ["meals"],
-  // 	queryFn: () => fetch("/api/meals").then((res) => res.json()),
-  // });
-
-  // console.log(meals)
-
   return (
-    // <HydrationBoundary state={dehydrate(queryClient)}>
     <div>
       <LinkButton href="/meals/add" className="mb-4">
         Add Meal
       </LinkButton>
       <Meals />
     </div>
-    // </HydrationBoundary>
   );
 }
