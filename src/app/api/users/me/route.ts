e6f0e0cc96a5b2c026fa8b1a47fd72dfc6918020
paul-mcn <paul.a.mcnamara@outlook.com>
2024-05-13 01:08:04 +1000

    added log to failed fetch on /users/me

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -4,14 +4,18 @@ import { cookies } from "next/headers";
 export async function GET() {
   const tokenString = cookies().get("token");
   const token: Token = JSON.parse(tokenString?.value || "{}");
-  const payload = await fetch(`${process.env.API_HOST}/users/me`, {
-    headers: {
-      Accept: "application/json",
-      authorization: `${token.token_type} ${token.access_token}`,
-    },
-    cache: "no-store",
-  });
-  const user = await payload.json();
+  try {
+    const payload = await fetch(`${process.env.API_HOST}/users/me`, {
+      headers: {
+        Accept: "application/json",
+        authorization: `${token.token_type} ${token.access_token}`,
+      },
+      cache: "no-store",
+    });
+    const user = await payload.json();
 
-  return new Response(JSON.stringify(user));
+    return new Response(JSON.stringify(user));
+  } catch (error) {
+    return new Response(JSON.stringify(error));
+  }
 }
