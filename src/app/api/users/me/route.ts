9d8af05b45e1457ff6a876540d12e22ea17a0bce
paul-mcn <paul.a.mcnamara@outlook.com>
2024-05-12 16:19:03 +1000

    fixed hardcoded url

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -2,13 +2,16 @@ import { Token } from "@/hooks/user";
 import { cookies } from "next/headers";
 
 export async function GET() {
-	const tokenString = cookies().get("token")
-	const token: Token = JSON.parse(tokenString?.value || "{}")
-	const payload = await fetch("http://localhost:8000/users/me", {
-		headers: { "Accept": "application/json", authorization: `${token.token_type} ${token.access_token}` },
-		cache: "no-store"
-	})
-	const user = await payload.json()
+  const tokenString = cookies().get("token");
+  const token: Token = JSON.parse(tokenString?.value || "{}");
+  const payload = await fetch(`${process.env.API_HOST}/users/me`, {
+    headers: {
+      Accept: "application/json",
+      authorization: `${token.token_type} ${token.access_token}`,
+    },
+    cache: "no-store",
+  });
+  const user = await payload.json();
 
-	return new Response(JSON.stringify(user));
+  return new Response(JSON.stringify(user));
 }
