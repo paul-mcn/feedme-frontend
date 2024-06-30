5ae79248c9a5f5b467def31118aa2890e97cebd9
paul-mcn <paul.a.mcnamara@outlook.com>
2024-06-30 16:55:48 +1000

    add hide routes unless user has auth token

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,6 @@
 "use client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 import AccountDropdown from "./AccountDropdown";
+import NavBarLinks from "./NavBarLinks";
 import { useUser } from "@/hooks/user";
 
 const links = [
@@ -16,35 +15,18 @@ const authLinks = [
 ];
 
 const NavBar = () => {
-  const router = usePathname();
-  const { data: user } = useUser();
+  const user = useUser();
+
+  const filteredLinks = user.isSuccess
+    ? links
+    : links.filter((link) => link.name === "Home");
 
   return (
     <div className="bg-white w-full h-[12vh] z-10">
       <div className="flex flex-row w-4/5 mx-auto h-full justify-between">
-        <ul className="flex flex-row items-center text-gray-900 gap-4 h-full font-bold">
-          {links.map((link) => (
-            <li
-              className={`${router === link.href ? "text-green-600" : "hover:opacity-80"}`}
-              key={link.href}
-            >
-              <Link href={link.href}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
+        <NavBarLinks links={filteredLinks} />
         <ul className="flex flex-row items-center text-gray-900 gap-4 h-full z-20">
-          {user ? (
-            <AccountDropdown />
-          ) : (
-            authLinks.map((link) => (
-              <li
-                className={`${router === link.href ? "text-green-600" : "hover:opacity-80"}`}
-                key={link.href}
-              >
-                <Link href={link.href}>{link.name}</Link>
-              </li>
-            ))
-          )}
+          {user.data ? <AccountDropdown /> : <NavBarLinks links={authLinks} />}
         </ul>
       </div>
     </div>
